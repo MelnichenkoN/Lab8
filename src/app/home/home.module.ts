80dba829038241a04b589f6f46ace24819f53be9
MelnichenkoN <melnichenko.nik111@gmail.com>
2023-05-28 22:40:33 +0300

    first commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MyformComponent } from '../myform/myform.component';
+import { ViewformComponent } from '../viewform/viewform.component';
+import { UpdateformComponent } from '../updateform/updateform.component';
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [HomePage,MyformComponent,ViewformComponent, UpdateformComponent]
+})
+export class HomePageModule {}
